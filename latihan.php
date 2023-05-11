cffaffdc39b0efb10bdf05d3c729c6319d03a149
INTRN <aryaintaran24@gmail.com>
2023-05-11 18:14:05 +0800

    variable

--- latihan.php
+++ latihan.php
@@ -1,5 +1,5 @@
 <?php
-    // Variabel tidak bisa diawali dengan angka & simbol, tapi boleh mengandung angka
+    // Variabel tidak bisa diawali dengan angka & simbol, tapi boleh mengandung angka & simbol
     $name = "Arya Intaran";
 
     echo $name;
