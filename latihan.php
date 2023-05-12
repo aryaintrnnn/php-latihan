3bb795fced2bd4af86248bb5c295188b14fb1bbe
INTRN <aryaintaran24@gmail.com>
2023-05-12 11:54:20 +0800

    Global Variable

--- latihan.php
+++ latihan.php
@@ -1,6 +1,17 @@
 <?php
     // Variabel tidak bisa diawali dengan angka & simbol, tapi boleh mengandung angka & simbol
-    $name = "Arya Intaran";
+    
+    $name = "Arya Intaran"; // Global Variable
+    $panjang = 5;
+    $lebar = 3;
 
     echo $name;
+    echo '<br>';
+
+    echo '<br>';
+    echo 'Panjangnya adalah :'  .$panjang;
+    echo '<br>';
+    echo 'Lebarnya adalah :'  .$lebar;
+
 ?>
+
