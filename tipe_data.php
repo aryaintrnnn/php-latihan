f3958e47a496e0e7328bb6bd57410963851e86ee
INTRN <aryaintaran24@gmail.com>
2023-05-12 16:04:54 +0800

    string

--- tipe_data.php
+++ tipe_data.php
@@ -1,3 +1,30 @@
 <?php
+    // String
+    // String pada umumnya ditandai dengan simbol ' atau "
+
+    // Cara mengecek string
+    var_dump("Hello World");
+    var_dump('10');
+
+    echo '<br>';
+    echo '<br>';
+
+    // Perbedaan menggunaan ' dan "
+    $hewan = "Kucing";
+
+    echo "Menggunakan petik 2";
+    echo "<br>";
+    var_dump("Saya punya $hewan"); //Variable akan dibaca sebagai kucing
+    echo "<br>";
+    echo "Menggunakan petik 1 "; 
+    echo "<br>";
+    var_dump('Saya punya $hewan'); //Variable tidak akan dibaca sebagai kucing melainkan $hewan
+
+    echo "<br>";
+
+    // Integer
+
     
+
+
 ?>
