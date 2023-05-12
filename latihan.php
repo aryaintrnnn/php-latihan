b9bbdf4e1f5112723500808685e2a9916f6acd25
INTRN <aryaintaran24@gmail.com>
2023-05-12 12:16:52 +0800

    static variable

--- latihan.php
+++ latihan.php
@@ -2,6 +2,9 @@
     // Variabel tidak bisa diawali dengan angka & simbol, tapi boleh mengandung angka & simbol
     // Global variable hanya bisa dikenali di luar function
     // Local variable hanya bisa dikenali di dalam function
+    // Static variable hanya bisa dikenali di dalam function, tapi nilainya tidak akan hilang meskipun functionnya sudah dijalankan
+
+    // Global Variable
     
     $name = "Arya Intaran"; // Global Variable
     $panjang = 5;
@@ -16,6 +19,7 @@
     echo 'Lebarnya adalah :'  .$lebar;
     echo '<br>';
 
+    // Local Variable
 
     function Panggil(){
         $sisi = 5; // Local Variable
@@ -41,14 +45,14 @@
     $phi = 3.14;
 
     function klllingkaran(){
-        global $phi;
+        global $phi; // Memanggil variabel global
         $r = 10;
         $kll = 2 * $phi * $r;
         echo $kll;
     }
 
     function klllingkaran2(){
-        global $phi;
+        global $phi; // Memanggil variabel global
         $r = 15;
         $kll = 2 * $phi * $r;
         echo $kll;
@@ -61,6 +65,22 @@
     echo '<br>';
     echo 'Keliling lingkaran 2 adalah: ';
     klllingkaran2();
+
+    // Static Variable
+
+    function panggil2(){
+        static $x = 1; // Static Variable
+        echo $x;
+        $x++; // x = x + 1
+    }
+
+    // Jika hanya memanggil 1x, maka nilai x = 1, tetapi jika memanggil 2x maka nilai x = 2 dan seterusna
+
+    echo '<br>';
+    echo '<br>';
+    panggil2(); 
+    echo '<br>';
+    panggil2();
 
 ?>
 
