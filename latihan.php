a84dc9ec669693002c55fe3fe7c3ef768bddc186
INTRN <aryaintaran24@gmail.com>
2023-05-12 12:02:08 +0800

    Local variable

--- latihan.php
+++ latihan.php
@@ -1,5 +1,7 @@
 <?php
     // Variabel tidak bisa diawali dengan angka & simbol, tapi boleh mengandung angka & simbol
+    // Global variable hanya bisa dikenali di luar function
+    // Local variable hanya bisa dikenali di dalam function
     
     $name = "Arya Intaran"; // Global Variable
     $panjang = 5;
@@ -12,6 +14,20 @@
     echo 'Panjangnya adalah :'  .$panjang;
     echo '<br>';
     echo 'Lebarnya adalah :'  .$lebar;
+    echo '<br>';
+
+
+    function Panggil(){
+        $sisi = 5; // Local Variable
+
+        echo '<br>';
+        echo 'Sisinya adalah: ' .$sisi;
+        echo '<br>';
+    }
+    
+    Panggil(); // Memanggil fungsi/function
+
+
 
 ?>
 
