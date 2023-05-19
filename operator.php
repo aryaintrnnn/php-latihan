1da932e34be6d9e4a4c8742259fb9286bf6460b4
INTRN <aryaintaran24@gmail.com>
2023-05-19 00:47:24 -0700

    // Arithmetic Operator

--- operator.php
+++ operator.php
@@ -1,7 +1,14 @@
 <?php
+    // Arithmetic Operator
     $a = 4;
     $b = 5;
 
-    $c = $a + $b;
+    $c = $a + $b; //Penjumlahan
+    $d = $a - $b; //Pengurangan
+    $e = $a * $b; //Perkalian
     echo $c;
+    echo '<br>';
+    echo $d;
+    echo '<br>';
+    echo $e;
 ?>
